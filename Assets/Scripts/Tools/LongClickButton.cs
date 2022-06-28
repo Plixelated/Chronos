c535b51326e5dee3785d83c8cdaa2d94e9b70609
Plixelated <76276169+Plixelated@users.noreply.github.com>
2022-06-28 02:33:16 -0700

    Started On Long Press Variation Functionality
    -Created long press script
    -Started adding UI elements

---
+++ Assets/Scripts/Tools/LongClickButton.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class LongClickButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    private bool pointerDown;
+    private float pointerDownTimer;
+
+    public float requiredHoldTime;
+
+    public UnityEvent onLongClick;
+
+    public void OnPointerDown(PointerEventData eventData)
+    { 
+        pointerDown = true;
+        Debug.Log("OnPointerDown");
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Reset();
+    }
+
+    private void Update()
+    {
+        if (pointerDown)
+        {
+            pointerDownTimer += Time.deltaTime;
+            if (pointerDownTimer > requiredHoldTime)
+            {
+                if (onLongClick != null)
+                {
+                    onLongClick.Invoke();
+                }
+
+                Reset();
+            }
+        }
+    }
+
+    private void Reset()
+    {
+        pointerDown = false;
+        pointerDownTimer = 0;
+    }
+}
